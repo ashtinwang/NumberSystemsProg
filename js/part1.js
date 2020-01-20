c89c8b7513428828ba58ee394ddecf8d4d2826d1
Ashtin <fooblyfu@gmail.com>
2020-01-20 01:46:12 -0800

    Part 1 Done and Part 2 work
    forgot to keep committing whoops

--- js/part1.js
+++ js/part1.js
@@ -1,14 +1,91 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
-    var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
-    var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
+  var UnsignedInt = document.getElementById("1_UnsignedInt").value;
+  var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
+  var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
 
+  var outputVal = "";
+  var value = 0;
+  var output = "";
+  var intArr = UnsignedInt.split("");
+  for (var c = 0; c < intArr.length; c++) {
+    var temp = parseGuy(intArr[c]);
+    value = (value * UnsignedIntBaseFrom) + temp;
+  }
+  output = value;
+  //REPEAT
+  //Value = (Value * N) + next digit
+  //UNTIL(no digits remain);
 
-  var outputValue = "11111111";
+  while (output != 0) {
+    var temp2 = output % UnsignedIntBaseTo;
+    if (UnsignedIntBaseTo > 10){
+      temp2 = reverseParseGuy(temp2);
+    }
+    outputVal = temp2 + outputVal;
+    output = Math.trunc(output / UnsignedIntBaseTo);
+  }
+  //Value := value to be converted;
+  //REPEAT
+  //Next digit of result := Value MOD N;
+  //Value := Value DIV N;
+  //UNTIL (Value = 0);
+
 
   // Show the output on the screen
-  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
+  FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputVal], 1);
 
+}
+
+function parseGuy(expression) {
+  switch (expression) {
+    case "A":
+      return 10;
+      break;
+    case "B":
+      return 11;
+      break;
+    case "C":
+      return 12;
+      break;
+    case "D":
+      return 13;
+      break;
+    case "E":
+      return 14;
+      break;
+    case "F":
+      return 15;
+      break;
+    default:
+      return parseInt(expression);
+      break;
+
+  }
+}
+function reverseParseGuy(expression) {
+  switch (expression) {
+    case 10:
+      return "A";
+      break;
+    case 11:
+      return "B";
+      break;
+    case 12:
+      return "C";
+      break;
+    case 13:
+      return "D";
+      break;
+    case 14:
+      return "E";
+      break;
+    case 15:
+      return "F";
+      break;
+    default:
+      return parseInt(expression);
+      break;
+    }
 }
--- js/part2.js
+++ js/part2.js
@@ -1,12 +1,72 @@
 function ConversionPart2() {
-    //
-    var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
+  //
+  var SignedDecimalInt = document.getElementById("2_SignedInt").value;
+
+  //I don't understand what "input format will be [-][0-9]+" is supposed to mean
+
+  var negative = false;
+  if (SignedDecimalInt.charAt(0) == "-") {
+    negative = true;
+  }
+
+  if (negative) {
+    var unsigned = parseInt(SignedDecimalInt.substring(1));
+  } else {
+    unsigned = parseInt(SignedDecimalInt);
+  }
+  var outputVal = "";
+  while (unsigned != 0) {
+    var temp = unsigned % 2;
+    outputVal = temp + outputVal;
+    unsigned = Math.trunc(unsigned / 2);
+  }
+  while (outputVal.length < 7) {
+    outputVal = 0 + outputVal;
+  }
+  if (negative == true) {
+    outputVal = 1 + outputVal;
+  } else {
+    outputVal = 0 + outputVal;
+  }
 
 
+  var intArr = outputVal.split("");
+  var outputVal2 = "";
+  for (var c = 0; c < intArr.length; c++) {
+    if (intArr[c] == 0) {
+      intArr[c] = 1;
+    } else if (intArr[c] == 1) {
+      intArr[c] = 0;
+    }
+    outputVal2 += intArr[c];
+  }
 
-    var outputValue = "010110101000110110011101";
-    var outputValueTwosComplement = "101001010111001001100011";
 
-    // Show the output on the screen
-    FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
+  var outputVal3 = "";
+  var z = intArr.length - 1;
+  var carry = false;
+
+  if (intArr[intArr.length - 1] == 0) {
+    intArr[intArr.length - 1] = 1;
+  } else if (intArr[intArr.length - 1] == 1) {
+    carry = true;
+    intArr[intArr.length - 1] = 0;
+    while (carry=true) {
+      if (intArr[i] == 0) {
+        intArr[i] = 1;
+        carry = false;
+      } else if (intArr[i] == 1) {
+        intArr[i]=0;
+      }
+    }
+  }
+  for (var i = intArr.length - 1; i > 0; i--) {
+    outputVal3 = intArr[i] + outputVal3;
+  }
+
+
+  FormatAndShowOutput([outputVal, outputVal3, SignedDecimalInt], 2);
+
+  // Show the output on the screen
+  //FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
 }

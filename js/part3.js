2640b54fdae6b292dbcae3fbae4f62cd2264b3c6
Ashtin <fooblyfu@gmail.com>
2020-01-21 00:05:22 -0800

    Part 3 works

--- js/part3.js
+++ js/part3.js
@@ -19,7 +19,7 @@ function ConversionPart3() {
 
   //Binary Conversion Decimal
   var output2 = "";
-  var input2 = output;
+  var input2 = floatDec;
   while (input2 != 0) {
     input2 = input2 * 2;
     output2 += Math.trunc(input2);
@@ -27,30 +27,98 @@ function ConversionPart3() {
   }
 
   //Mantissa Maker
-  mantissa += output;
+  mantissa += output.substring(1);
   var decArr = output2.split("");
-  while (mantissa.length < 23) {
+  while (mantissa.length < 22) {
     for (var c = 0; c < decArr.length; c++) {
       mantissa += decArr[c];
     }
+    if (mantissa.length == 22) {
+      mantissa = 1 + mantissa;
+    }
+    if (mantissa.length != 23) {
+      mantissa = 0 + mantissa;
+    }
   }
 
-  //
-
-  //Value := fraction to be converted;
-  //REPEAT Value := Value * N;
-  //Next digit of result := integer part of Value;
-  //Value := fractional part of Value;
-  //UNTIL (Value = 0) or (the desired number of digits are produced);
 
 
+  //Exponent
+  var exponent = 0;
+  if (parseInt(output) != 0) {
+    exponent = output.length - 1;
+  } else {
+    var i = 0;
+    while (output2.charAt(i) != 0) {
+      i++;
+    }
+    exponent = -i - 1;
+  }
+  exponent += 128;
+  if (exponent < 0) {
+    var negExponent = true;
+    exponent = Math.abs(exponent);
+  }
+  var outputEx = "";
+  while (exponent != 0) {
+    var temp2 = exponent % 2;
+    outputEx = temp2 + outputEx;
+    exponent = Math.trunc(exponent / 2);
+  }
+  if (negExponent == true) {
+    var intArr = outputEx.split("");
+    var output3 = "";
+    for (var c = 0; c < intArr.length; c++) {
+      if (intArr[c] == 0) {
+        intArr[c] = 1;
+      } else if (intArr[c] == 1) {
+        intArr[c] = 0;
+      }
+      output3 += intArr[c];
+    }
 
+    var z = intArr.length - 1;
+    var carry = false;
+    var intArr2 = output3.split("");
+    var outputVal3 = "";
+    if (intArr2[z] == 0) {
+      intArr2[z] = 1;
+      for (var i = z; i >= 0; i--) {
+        outputVal3 = intArr2[i] + outputVal3;
+      }
+    } else if (intArr2[z] == 1) {
+      carry = true;
+      intArr2[z] = 0;
+      z--;
+      while (carry == true) {
+        if (intArr2[z] == 0) {
+          intArr2[z] = 1;
+          carry = false;
+        } else if (intArr2[z] == 1) {
+          intArr2[i] = 0;
+        }
+      }
+      for (var q = z; q >= 0; q--) {
+        outputVal3 = intArr2[q] + outputVal3;
+      }
+    }
+  }
+  var isNeg = 0;
+  if (negative == true) {
+    isNeg = 1;
+  }
 
-  var output32BitScientificNotation = "10100011001100001000010100101010";
+if (negExponent == true){
+  var output32BitScientificNotation = mantissa + outputVal3 + isNeg;
+}
+else{
+  var output32BitScientificNotation = mantissa + outputEx + isNeg;
+}
 
   // Show the output on the screen
   FormatAndShowOutput([floatToConvert, output32BitScientificNotation], 3);
 }
+
 
 
 // If you dare read a comment before starting to program..
@@ -65,4 +133,11 @@ function ConversionPart3() {
 // ... so for bits (0-22) 10100011001100001000010
 // ... so exponent representation in +128 format is 21+128 = 149 = (bits 23-30) 10010101
 // ... so final sign bit = (bit 31) 0
-//
+//11010001100110000100001100101010
+//11010001100110000100001100101010
+
+//Value := fraction to be converted;
+//REPEAT Value := Value * N;
+//Next digit of result := integer part of Value;
+//Value := fractional part of Value;
+//UNTIL (Value = 0) or (the desired number of digits are produced);
